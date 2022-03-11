bca00b52296e099edb7b4505b4ff1e3193339f7c
Ben Hansen <bhbenjaminhansen@gmail.com>
2022-03-10 18:13:26 -0700

    mitten is using arm's skeleton

--- src/index.js
+++ src/index.js
@@ -1,35 +1,57 @@
 import './style/main.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-/**
- * GUI Controls
- */
-import * as dat from 'dat.gui'
-const gui = new dat.GUI()
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { SkeletonUtils } from 'three/examples/jsm/utils/SkeletonUtils'
 
-/**
- * Base
- */
-// Canvas
+// import * as dat from 'dat.gui'
+// const gui = new dat.GUI()
+
+// https://discourse.threejs.org/t/how-to-put-a-weapon-in-a-characters-hand/22121/4
+
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.IcosahedronGeometry(20, 1)
-const material = new THREE.MeshNormalMaterial()
-// Material Props.
-material.wireframe = true
-// Create Mesh & Add To Scene
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const light = new THREE.DirectionalLight(0xffffff, 10);
+scene.add(light);
 
-/**
- * Sizes
- */
+let arm, mitten, armSkeleton, mittenSkeleton;
+async function loadModels() {
+  const loader = new GLTFLoader();
+  arm = await loader.loadAsync("/Low Poly Arm.glb");
+  mitten = await loader.loadAsync("/Mitten.glb");
+  scene.add(arm.scene);
+  scene.add(mitten.scene);
+
+  console.log(arm.scene);
+
+  // Find the skeleton
+  let armSkeleton;
+  arm.scene.traverse(node => {
+    if (node.isSkinnedMesh) {
+      armSkeleton = node.skeleton;
+      return;
+    }
+  })
+  let mittenSkeleton;
+  mitten.scene.traverse(node => {
+    if (node.isSkinnedMesh) {
+      // mittenSkeleton = node.skeleton;
+      node.skeleton = armSkeleton;
+      return
+    }
+  })
+
+  arm.mixer = new THREE.AnimationMixer(arm.scene);
+  arm.animations.forEach(clip => {
+    console.log("clip:", clip)
+    arm.mixer.clipAction(clip).play();
+  })
+}
+
+
+loadModels();
+
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -49,37 +71,28 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.001,
-  5000
+  100
 )
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 50
+camera.position.z = 1
 scene.add(camera)
 
-// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.autoRotate = true
-// controls.enableZoom = false
 controls.enablePan = false
 controls.dampingFactor = 0.05
-controls.maxDistance = 1000
-controls.minDistance = 30
+controls.maxDistance = 10
+controls.minDistance = 0.1
 controls.touches = {
   ONE: THREE.TOUCH.ROTATE,
   TWO: THREE.TOUCH.DOLLY_PAN,
 }
-/**
- * Renderer
- */
+
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
@@ -87,23 +100,19 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
 
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  //mesh.rotation.y += 0.01 * Math.sin(1)
-  mesh.rotation.z += 0.01 * Math.sin(1)
-
-  // Update controls
   controls.update()
-  // Render
   renderer.render(scene, camera)
 
-  // Call tick again on the next frame
+  if (arm && arm.mixer) {
+    arm.mixer.update(1 / 64);
+
+    // if (armSkeleton && mittenSkeleton) {
+    //   SkeletonUtils.retarget(mittenSkeleton, armSkeleton);
+    // }
+  }
+
   window.requestAnimationFrame(tick)
 }
 
